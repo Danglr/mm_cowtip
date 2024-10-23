85f0004478ec7a3d381c1b011820a8afcdbcaa69
Danglr <109924850+Danglr@users.noreply.github.com>
2024-10-23 15:07:00 -0500

    Update cl.lua
    Edited original code with another authors for optimizing.

--- cl.lua
+++ cl.lua
@@ -1,63 +1,46 @@
 -- RedM Script to ragdoll a cow when colliding with it
 
 -- Constants
-local COW_MODEL = "a_c_cow"
+local COW_MODEL = `a_c_cow` -- Let's hash this right away, so we arent running GetHashKey in a loop for the model check.
 local DETECT_DISTANCE = 1.5 -- Distance to detect the cow
 local RAGDOLL_TIME = 5000 -- Duration of the ragdoll effect in milliseconds
 
--- Function to check if the player is close to a cow
-function IsNearCow()
-    local playerPed = PlayerPedId()
-    local playerPos = GetEntityCoords(playerPed)
-    
-    -- Get a list of all nearby entities
-    local entities = GetGamePool("CPed")
-    
-    for _, entity in ipairs(entities) do
-        if IsPedAPlayer(entity) then
-            -- Skip if the entity is a player
-            goto continue
-        end
-        
-        local entityPos = GetEntityCoords(entity)
-        local distance = Vdist(playerPos.x, playerPos.y, playerPos.z, entityPos.x, entityPos.y, entityPos.z)
-        
-        if distance < DETECT_DISTANCE and GetEntityModel(entity) == GetHashKey(COW_MODEL) then
-            return entity
-        end
-        
-        ::continue::
-    end
-    
-    return nil
-end
-
--- Function to apply ragdoll effect to the cow
-function RagdollCow()
-    local cow = IsNearCow()
-    
-    if cow then
-        -- Set the cow to ragdoll
-        SetPedToRagdoll(cow, RAGDOLL_TIME, RAGDOLL_TIME, 0, false, false, false)
-    end
-end
-
 -- Main thread
-Citizen.CreateThread(function()
+CreateThread(function() -- Citizen.CreateThread = CreateThread
     while true do
-        Citizen.Wait(0)
-        
-        -- Check if the player is colliding with a cow
-        local cow = IsNearCow()
-        if cow then
-            -- Get the player's velocity
-            local playerPed = PlayerPedId()
-            local playerVelocity = GetEntityVelocity(playerPed)
-            
-            -- If the player is moving fast enough, ragdoll the cow
-            if Vmag(playerVelocity) > 5.0 then -- Adjust the speed threshold as needed
-                RagdollCow()
+        -- Slow down the interval A LOT, this is a very expensive loop operation. 
+        -- This should be slow enough for a huge performance boost, while still being fast enough to detect cows player is running against.
+        Wait(400)
+
+        local playerPed = PlayerPedId()
+        local playerPos = GetEntityCoords(playerPed)
+
+        for _, entity in ipairs(GetGamePool('CPed')) do
+            -- Let's add the model check here, so we don't have to loop through all entities AND check their distance before we make sure its actually a cow.
+            -- I got rid of the IsPedAPlayer check since player's can't be cows (under normal circumstances).
+            if GetEntityModel(entity) ~= COW_MODEL then
+                -- Skip if the entity is a player or not a cow
+                goto continue
             end
+
+            local entityPos = GetEntityCoords(entity)
+            local distance = #(playerPos - entityPos) -- This distance check is much, much faster.
+
+            if distance < DETECT_DISTANCE then -- Here we only need to check the distance, since we already know we're dealing with a cow.
+                -- Get the player's velocity
+                local playerSpeed = GetEntitySpeed(playerPed) -- If we get the speed, we don't need to calculate magnitude on a vector.
+
+                -- If the player is moving fast enough, ragdoll the cow
+                if playerSpeed > 5.0 then
+                    -- Let's get rid of the RagdollCow function since it only had one line of code after optimization.
+                    SetPedToRagdoll(entity, RAGDOLL_TIME, RAGDOLL_TIME, 0, false, false, false)
+                    -- Let's introduce a timeout because we don't need to keep checking for a while immediately after ragdolling the cow.
+                    Wait(2000) -- Wait 2 seconds before continuing, and then break the for loop because we found our cow.
+                    break
+                end
+            end
+
+            ::continue::
         end
     end
 end)
